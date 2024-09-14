2d0c6fc843eed1836247fcfd55ecc9e0b01fbbbd
Mohamed Aziz Khayati <mohamed.aziz.khayati@divizend.com>
2024-09-14 16:20:45 +0200

    fix(server): draw board should have a state

--- server/src/handlers/draw.handler.ts
+++ server/src/handlers/draw.handler.ts
@@ -1,7 +1,11 @@
 import { Socket, Server } from "socket.io";
 
+let drawBoard: any[] = [];
+
 export const drawHandler = async (io: Server, socket: Socket) => {
+  socket.emit("draw", drawBoard);
   socket.on("draw", (msg) => {
+    drawBoard = msg;
     io.emit("draw", msg);
   });
 };
